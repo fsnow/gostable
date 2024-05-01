ce82b6a04eba2ff76ee1cfb68288186d4b831632
Frank Snow <fsnow@fsnow.com>
2024-05-01 10:44:05 -0400

    Added "golden" files (expected output) and bash script to test/compare

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.21
 
 toolchain go1.22.2
 
+require golang.org/x/tools v0.20.0
+
 require (
-	golang.org/x/mod v0.17.0
-	golang.org/x/sync v0.7.0
-	golang.org/x/tools v0.20.0
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 )
--- test.sh
+++ test.sh
@@ -1,20 +1,76 @@
 ./build.sh
 
 cd testdata/unstable/
-# https://github.com/golang/go/issues/37054#issuecomment-1648091135
-# this is a workaround to resolve dependencies of the test code
-#go mod tidy && go mod vendor && go mod verify
 go build
-GOSTABLE_OUT=`../../gostable .`
 
-echo "before"
-echo $GOSTABLE_OUT
-echo "after"
+GOSTABLE_OUT=$(../../gostable . 2>&1)
+
+# Initialize the CLIPPED_OUT variable
+CLIPPED_OUT=""
+
+# Process the output line by line
+while IFS= read -r line; do
+    # Remove the path before "gostable/testdata"
+    modified_line="${line#*/gostable/testdata}"
+    
+    # Prepend "gostable/testdata" to the modified line
+    modified_line="gostable/testdata$modified_line"
+    
+    # Append the modified line to CLIPPED_OUT
+    CLIPPED_OUT+="$modified_line"$'\n'
+done < <(echo "$GOSTABLE_OUT")
+
+# Remove the trailing newline character from CLIPPED_OUT
+CLIPPED_OUT="${CLIPPED_OUT%$'\n'}"
+
+#echo "Modified output:"
+#echo "$CLIPPED_OUT"
+
+# Compare CLIPPED_OUT with the contents of the file
+diff -u <(echo "$CLIPPED_OUT") golden
+
+# Check the exit status of the diff command
+if [ $? -eq 0 ]; then
+    echo "gostable output matches testdata/unstable/golden"
+else
+    echo "gostable output does not match testdata/unstable/golden"
+fi
 
 cd ../stable
-#go mod tidy && go mod vendor && go mod verify
 go build
-../../gostable .
+
+GOSTABLE_OUT=$(../../gostable . 2>&1)
+
+# Initialize the CLIPPED_OUT variable
+CLIPPED_OUT=""
+
+# Process the output line by line
+while IFS= read -r line; do
+    # Remove the path before "gostable/testdata"
+    modified_line="${line#*/gostable/testdata}"
+    
+    # Prepend "gostable/testdata" to the modified line
+    modified_line="gostable/testdata$modified_line"
+    
+    # Append the modified line to CLIPPED_OUT
+    CLIPPED_OUT+="$modified_line"$'\n'
+done < <(echo "$GOSTABLE_OUT")
+
+# Remove the trailing newline character from CLIPPED_OUT
+CLIPPED_OUT="${CLIPPED_OUT%$'\n'}"
+
+#echo "Modified output:"
+#echo "$CLIPPED_OUT"
+
+# Compare CLIPPED_OUT with the contents of the file
+diff -u <(echo "$CLIPPED_OUT") golden
+
+# Check the exit status of the diff command
+if [ $? -eq 0 ]; then
+    echo "gostable output matches testdata/stable/golden"
+else
+    echo "gostable output does not match testdata/stable/golden"
+fi
 
 
 cd ..
